95776c17f59adfee42b12e3da2994474b87f092b
Vladimir Kalinichev <vladimir.kalinichev@kaspersky.com>
2016-02-19 15:46:52 +0300

    Replaces in every h1,h1,h3 child and punctuation processing added

--- js/injection.js
+++ js/injection.js
@@ -1,13 +1,14 @@
 'use strict';
 
 (function () {
-    var headers = document.querySelectorAll( 'h1,h2,h3' );
+    var headers = document.querySelectorAll( 'h1,h2,h3,h1 *,h2 *,h3 *' );
 
     for (var i=0; i<headers.length; i++) {
         var header = headers[i],
             testString = header.innerText,
             isHow,
-            isAlreadyWhyed;
+            isAlreadyWhyed,
+            html;
 
         if ( testString ) {
             testString = testString.trim();
@@ -15,7 +16,11 @@
             isAlreadyWhyed = testString.match( /зачем\?$/i );
 
             if ( isHow && !isAlreadyWhyed) {
-                header.innerHTML = header.innerHTML + ', а главное - <span style="color:#c04">зачем?</span>'
+                html = header.innerHTML;
+                if ( html && html.match( /(:|,|\.|!|\?|\\|\/)$/ ) ) {
+                    html = html.slice( 0, -1 )
+                }
+                header.innerHTML = html + ', а главное - <span style="color:#c04">зачем?</span>'
             }
         }
     }
